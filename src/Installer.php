425e650a70523950b59adeaa12852db13e489976
Prihod <prihod2004@gmail.com>
2025-05-08 22:04:34 +0300

    update

--- src/Installer.php
+++ src/Installer.php
@@ -1,18 +1,25 @@
 <?php
+
 namespace MetaStorm;
 
 class Installer
 {
     public static function postInstall()
     {
-        $projectRoot = getcwd();
-        $schemaPath = "{$projectRoot}/core/model/schema";
+        echo "Running MetaStorm post-install script...\n";
+        $projectRoot = dirname(__DIR__, 4);
 
-        if(file_exists($schemaPath)) {
-            $force = getenv('FORCE') === 'true';
-            $templatePath = __DIR__ . '/../templates/meta-storm.xml.tpl';
-
-            MetaGenerator::generate($schemaPath, $templatePath, $force);
+        if (!file_exists($projectRoot . '/core/components')) {
+            echo "MODX structure not detected, skipping initial generation.\n";
+            return;
         }
+
+        echo "MODX structure detected, generating .meta-storm.xml files...\n";
+
+        $templatePath = dirname(__DIR__) . '/templates/meta-storm.xml.tpl';
+
+        MetaGenerator::generate($projectRoot, $templatePath, true, true);
+
+        echo "MetaStorm post-install process completed.\n";
     }
 }
